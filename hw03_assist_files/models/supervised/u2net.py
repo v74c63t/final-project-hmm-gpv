a0433216887d08d2473637bfa0f4aeca88190173
Vanessa Tang <vanesst5@uci.edu>
2024-03-12 14:06:43 -0700

    Add more template code

--- hw03_assist_files/models/supervised/u2net.py
+++ hw03_assist_files/models/supervised/u2net.py
@@ -4,6 +4,10 @@ import torch.nn as nn
 ## TODO
 
 class U2Net(nn.Module):
-    def __init__(self, *args, **kwargs) -> None:
-        super().__init__(*args, **kwargs)
-        # add args, etc
+    def __init__(self, in_channels: int, out_channels: int, **kwargs) -> None:
+        super(U2Net, self).__init__()
+        # add more args later
+    
+    def forward(self, x):
+        ## TODO
+        return
