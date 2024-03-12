cd5538beb2629b5993be87a6d7d45393bb66e124
Vanessa Tang <vanesst5@uci.edu>
2024-03-12 13:51:18 -0700

    Added initial template

--- hw03_assist_files/models/supervised/u2net.py
+++ hw03_assist_files/models/supervised/u2net.py
@@ -2,3 +2,8 @@ import torch
 import torch.nn as nn
 
 ## TODO
+
+class U2Net(nn.Module):
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+        # add args, etc
