4414b8055c19e92b1e461d0d7275f9971668e392
Vanessa Tang <vanesst5@uci.edu>
2024-03-12 15:44:35 -0700

    Minor additions

--- hw03_assist_files/models/supervised/u2net.py
+++ hw03_assist_files/models/supervised/u2net.py
@@ -3,10 +3,14 @@ import torch.nn as nn
 
 ## TODO
 
+# 6 encoders, 5 decoders
+
 class U2Net(nn.Module):
     def __init__(self, in_channels: int, out_channels: int, **kwargs) -> None:
         super(U2Net, self).__init__()
-        # add more args later
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        # TODO add more args later
     
     def forward(self, x):
         ## TODO
