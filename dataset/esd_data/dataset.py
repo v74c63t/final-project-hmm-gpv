d8360096a199d30a1eecf78538403031ceddb8b4
Yihong Yu <yihony1@uci.edu>
2024-03-21 23:24:17 -0700

    clean up dataset/dataset.py

--- dataset/esd_data/dataset.py
+++ dataset/esd_data/dataset.py
@@ -10,6 +10,7 @@ from pathlib import Path
 from ..preprocessing.subtile_esd_hw02 import Subtile, TileMetadata
 from typing import List, Dict
 from copy import deepcopy
+
 class DSE(Dataset):
     """
     Custom dataset for the IEEE GRSS 2021 ESD dataset.
@@ -33,11 +34,8 @@ class DSE(Dataset):
     def __init__(self, root_dir: str | os.PathLike, selected_bands: Dict[str, List[str]] | None=None, transform=None):
         self.root_dir = Path(root_dir)
         self.tiles = list(self.root_dir.glob("*.npz"))
-        # print(f"Loading tiles from: {self.root_dir}")
-        # print(f"Found {len(self.tiles)} subtiles.") 
         self.selected_bands = selected_bands
         self.transform = transform
-
 
     def __len__(self):
         """
@@ -118,8 +116,6 @@ class DSE(Dataset):
                 indices = self.__select_indices(satellite_bands, selected_bands)
                 new_metadata.satellites[key] = subtile.tile_metadata.satellites[key]
                 subtile.tile_metadata.satellites[key].bands = self.selected_bands[key]
-                # for i in indices:
-                #   selected_satellite_stack[key][:, i, :, :] = subtile.satellite_stack[key][:, i, :, :]
                 selected_satellite_stack[key] = subtile.satellite_stack[key][:, indices, :, :] # dimensions [t, bands, h, w]
         else:
             selected_satellite_stack = subtile.satellite_stack
@@ -151,36 +147,20 @@ class DSE(Dataset):
         """
         # load the subtiles using the Subtile class in
         # src/preprocessing/subtile_esd_hw02.py
-
+        tile_path = self.tiles[idx]
+        subtile = Subtile().load(tile_path)
 
         # call the __select_bands function to select the bands and satellites
+        selected_satellite_stack, _ = self.__select_bands(subtile)
 
         # stack the time dimension with the bands, this will treat the
         # timestamps as bands for the model you may want to change this
         # depending on your model and depending on which timestamps and
         # bands you want to use
-
         # Concatenate the time and bands
-
-        # Adjust the y ground truth to be the same shape as the X data by
-        # removing the time dimension
- 
-
-        # all timestamps are treated and stacked as bands
-
-        # if there is a transform, apply it to both X and y
-        tile_path = self.tiles[idx]
-        subtile = Subtile().load(tile_path)
-
-        selected_satellite_stack, _ = self.__select_bands(subtile)
         X = None
-        # y = None
         y = subtile.satellite_stack['gt']
-
         y = y-1
-        # (1,1,1,1)
-        # print("SS_Stack")
-        # print(selected_satellite_stack)
 
         for key, stack in selected_satellite_stack.items():
             if key != 'gt': 
@@ -188,22 +168,16 @@ class DSE(Dataset):
                     X = self.__aggregate_time(stack)
                 else:
                     X = np.concatenate((X, self.__aggregate_time(stack)), axis=0)
-            # else:
-        y = self.__aggregate_time(y)#[0, :, :] # now (1,1,1)
-        # print("DATASET y shape test: ")
-        # print(y.shape)  # TEST
+
+        # Adjust the y ground truth to be the same shape as the X data by
+        # removing the time dimension
+        y = self.__aggregate_time(y) #[0, :, :], now (1,1,1)
+
+        # if there is a transform, apply it to both X and y
         if self.transform:
-            # print(f"Data SET X: {X.shape}")
-            # print(f'Data SET y: {y.shape}')
             tranDict = self.transform({'X': X, 'y': y})
-            # X = self.transform(X)
-            # y = self.transform(y)
-            
             X = tranDict['X']
             y = tranDict['y']
-
-            # print(f'Data SET AFTER Transform X: {X.shape}')
-            # print(f'Data SET AFTER Transform y: {y.shape}')
             
         return X, y, subtile.tile_metadata
 
